35a9f007586dfca8dfeb4b736fc4b143c30e7308
mohamedelamineboumezzou-alt <mohamedelamineboumezzou@gmail.com>
2026-03-07 13:49:34 +0100

    initripo

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mobile_socode/setram_app.dart';
 
 void main() {
-  
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(title:Text("Title")),
-      body : Text("Start"),)
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const SetramApp());
 }
---
+++ lib/setram_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetramApp extends StatefulWidget {
+  const SetramApp({super.key});
+
+  @override
+  State<SetramApp> createState() => _SetramAppState();
+}
+
+class _SetramAppState extends State<SetramApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
